737a9917589b6112f2cb5fd8bb02cb26924bb8ef
corti009 <co28109@email.vccs.edu>
2025-08-02 03:31:30 +0000

    This is my Patterns program.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
 #include <iostream>
 
-int main() { std::cout << "Hello World!\n"; }
+int main() {
+    std::cout << "Pattern A:\n";
+    for (int row = 1; row <= 10; ++row) {
+        for (int col = 0; col < row; ++col) {
+            std::cout << "+";
+        }
+        std::cout << std::endl; 
+    }
+
+    std::cout << "\nPattern B:\n";
+    for (int row = 10; row >= 1; --row) {
+        for (int col = 0; col < row; ++col) {
+            std::cout << "+";
+        }
+        std::cout << std::endl; 
+    }
+
+    return 0;
+}
