2fa0f26c6a9a36b12886da16596e8784c13ad0a2
corti009 <co28109@email.vccs.edu>
2025-08-02 02:22:13 +0000

    This is my Student Lineup program.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,24 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
-int main() { std::cout << "Hello World!\n"; }
+int main() {
+    std::vector<std::string> studentNames = {
+        "Jackie", "Sam", "Tom", "Bill", "Mary", "Paul", "Zev", "Barb"
+    }; 
+
+    if (studentNames.empty()) {
+        std::cout << "No students found in the lineup." << std::endl;
+        return 0; 
+    }
+    int totalStudents = studentNames.size();
+    std::string frontOfLine = *std::min_element(studentNames.begin(), studentNames.end());
+    std::string endOfLine = *std::max_element(studentNames.begin(), studentNames.end());
+
+    std::cout << "Total number of students: " << totalStudents << std::endl; 
+    std::cout << "Student at the front of the line: " << frontOfLine << std::endl; 
+    std::cout << "Student at the end of the line: " << endOfLine << std::endl; 
+
+    return 0;
+}
